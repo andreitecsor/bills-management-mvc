69c74f3094f407e8f3c0759f2239ebabfd984b33
andreitecsor <andrei.tecsor@gmail.com>
2022-05-25 16:40:20 +0300

    Added bootstrap + admin context

--- EnterpriseBillsManagement/Data/ApplicationDbContext.cs
+++ EnterpriseBillsManagement/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using EnterpriseBillsManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EnterpriseBillsManagement.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<Bill> Bills { get; set; }
--- EnterpriseBillsManagement/Program.cs
+++ EnterpriseBillsManagement/Program.cs
@@ -1,4 +1,5 @@
 using EnterpriseBillsManagement.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace EnterpriseBillsManagement
@@ -8,21 +9,33 @@ namespace EnterpriseBillsManagement
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             builder.Services.AddControllersWithViews();
+
             builder.Services.AddDbContext<ApplicationDbContext>(opts =>
             {
                 opts.UseSqlServer(
                 builder.Configuration["ConnectionStrings:DefaultConnection"]);
             });
 
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+          .AddEntityFrameworkStores<ApplicationDbContext>();
+
             builder.Services.AddScoped<IBillRepository, BillRepository>();
             builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
 
             var app = builder.Build();
             app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapRazorPages();
             app.MapControllerRoute("pagination", "Bills/Page{productPage}", new { Controller = "Home", action = "Index" });
             app.MapDefaultControllerRoute();
             SeedData.EnsurePopulated(app);
+            Task.Run(async () =>
+            {
+                await SeedDataIdentity.EnsurePopulatedAsync(app);
+            }).Wait();
             app.Run();
         }
     }
